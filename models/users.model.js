9161e5feaae77bd6ca5238382537e46c912d9911
Ting Wong <ting_wong2@yahoo.fr>
2022-01-18 19:29:09 +0100

    added all routes CRUD

--- models/users.model.js
+++ models/users.model.js
@@ -2,27 +2,30 @@ const mongoose = require('mongoose');
 const { model, Schema } = mongoose;
 
 const usersSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
+    firstName: String,
+  
+    lastName: String,
+
+    age: Number,
+
     role: {
         type: String,
         enum: ["babysitter", "family"]
     },
-    password: {
-        type: String,
-        required: true,
-    },
+    password: String,
+    
     email: {
-        type: String,
-        required: true,
-    },
-    telephone: {
+		type: String,
+		unique: true,
+		validate: (email) => {
+			return Boolean(
+				email.match(
+					/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+				)
+			);
+		},
+	},
+    cellphone: {
         type: Number,
         validate: {
             validator: function(v) {
@@ -31,28 +34,18 @@ const usersSchema = new Schema({
             message: '{VALUE} is not a valid 10-digit number!'
         }
     },
-    picture: {
-        type: String,
-        required: true,
-    },
-    experience: {
-        type: Number,
-        //required: true,
-    },
-    resume: {
-        type: String,
-    },
-
+    picture: String,
+       
+    experience: Number,
+  
+    resume: String,
+    
     description: String,
 
-    numberOfKids: {
-        type: Number,
-        //required: true,
-    },
-    kidsAge: {
-        type: Number,
-        //required: true
-    },
+    numberOfKids: Number,
+   
+    kidsAge: Number,
+    
 
     availability: {
         type: [String],
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,18 +1,85 @@
 const router = require("express").Router();
 const Users = require("./../models/users.model");
 const mongoose = require("mongoose");
+const async = require("hbs/lib/async");
 
+router.get("/", (req, res, next) => {
+  const otherRole = !req.session.currentUser.role;
+  Users.find({ role: otherRole })
+    .then((users) => {
+      res.render("users/usersList", {
+        users: users,
+        css: ["users"],
+      });
+    })
+    .catch(next);
+});
 
+router.get("/:id", (req, res, next) => {
+  console.log(req.params);
+  const isValidId = mongoose.isValidObjectId(req.params.id);
+  const id = req.params.id;
+  if (isValidId) {
+    Users.findById(id)
+      .then((user) => {
+        console.log(user);
 
-router.get("/users", (req, res,next) => {
-	Users.find()
-		.then((users) => {
-			res.render("users/uersList.hbs", {
-				babysitters: babysitters,
-				css: ["users"],
+        res.render("users/oneUser.hbs", {
+          user: user,
+          css: ["users"],
+        });
+      })
+      .catch((e) => console.error(e));
+  } else {
+    next();
+  }
+});
+
+router.get("/create", (req, res, next) => {
+  res.render("users/createUser.hbs");
+});
+
+router.post("/create", (req, res) => {
+  console.log(req.body);
+	Users.create(req.body)
+    .then((newUser) => {
+      console.log("NewUser: ", newUser);
+      res.redirect("/users");
+    })
+    .catch((e) => console.error(e));
+});
+
+router.get("/id/delete", async (req, res) => {
+	const id = req.params.id;
+	try {
+		await Users.findByIdAndDelete(id);
+		res.redirect("/users");
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+router.get("/:id/update", (req, res) => {
+	const id = req.params.id;
+
+	Users.findById(id)
+		.then((foundUser) => {
+			res.render("users/updateUser.hbs", {
+				user: foundUser,
 			});
 		})
-		.catch(next);
+		.catch((e) => console.error(e));
 });
+
+router.post("/:id/update", (req, res) => {
+	const id = req.params.id;
+	Users.findByIdAndUpdate(id, req.body, { new: true })
+		.then((updatedUser) => {
+			console.log(updatedUser);
+			res.redirect("/users/" + id);
+		})
+		.catch((e) => console.error(e));
+});
+
 
 module.exports = router;
