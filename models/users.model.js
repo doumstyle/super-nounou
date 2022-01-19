b94345be790ff52e6a2558ac71087a16a3c1699f
Mamadou Ngatte <doumstyle@gmail.com>
2022-01-19 11:18:08 +0100

    Added CSS to createUser and updated userModel & route

--- models/users.model.js
+++ models/users.model.js
@@ -25,15 +25,8 @@ const usersSchema = new Schema({
 			);
 		},
 	},
-    cellphone: {
-        type: Number,
-        validate: {
-            validator: function(v) {
-                return /d{10}/.test(v);
-            },
-            message: '{VALUE} is not a valid 10-digit number!'
-        }
-    },
+    cellphone: String,
+
     picture: String,
        
     experience: Number,
