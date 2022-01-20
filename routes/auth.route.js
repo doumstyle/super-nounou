97ca70fafc9fc20e02aa25182bf2171b60d90e23
Hernan Menajovsky <hmenajovsky@gmail.com>
2022-01-20 12:44:15 +0100

    Splitted users for babysitters and families

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,88 +1,77 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const bcrypt = require("bcrypt"); 
+const bcrypt = require("bcrypt");
 const userModel = require("../models/users.model");
 
-router.get('/signin', function(req, res, next) {
-  res.render('auth/signin');
+router.get("/signin", function(req, res, next) {
+  res.render("auth/signin");
 });
 
+router.post("/signin", async (req, res, next) => {
+  try {
+    // create contants for model, password and email
+    const { email, password } = req.body;
 
-router.post('/signin', async (req, res, next) => {
-  try {      
-      // create contants for model, password and email
-      const { email, password } = req.body;
-    
-      //check if e-mail sent matches the e-mail in the collections
-      const foundUser = await userModel.findOne({ email: email });
+    //check if e-mail sent matches the e-mail in the collections
+    const foundUser = await userModel.findOne({ email: email });
 
-      if (!foundUser) {
+    if (!foundUser) {
+      req.flash("error", "Invalid credentials");
+      res.redirect("/auth/signin");
+    } else {
+      const isSamePassword = bcrypt.compareSync(password, foundUser.password);
+      if (!isSamePassword && password !== foundUser.password) {
         req.flash("error", "Invalid credentials");
         res.redirect("/auth/signin");
+      } else {
+     
+        const userObject = foundUser.toObject();
+        delete userObject.password;
+        req.session.currentUser = userObject;
+        req.flash("success", "Successfully logged in...");
+        res.redirect("/users");
+      }
     }
-    else {
-        const isSamePassword = bcrypt.compareSync(password, foundUser.password);
-           if (!isSamePassword && password !== foundUser.password) { 
-            req.flash("error", "Invalid credentials");
-            res.redirect("/auth/signin");
-        }
-        else {
-            const userObject = foundUser.toObject();
-            delete userObject.password;
-            req.session.currentUser = userObject;
-            req.flash("success", "Successfully logged in...");
-            res.redirect("/users");
-        }
-    }
-    }
-    catch (err) {
-        next(err);
-    }
-
-    });
-
-
-router.get('/signup', function(req, res, next) {
-  res.render('auth/signup');
+  } catch (err) {
+    next(err);
+  }
 });
 
+router.get("/signup", function(req, res, next) {
+  res.render("auth/signup");
+});
 
-router.post('/signup', async (req, res) => {  
+router.post("/signup", async (req, res) => {
   try {
     const newUser = { ...req.body };
-    
-      const foundUser = await userModel.findOne({ email: newUser.email });
 
-      if (foundUser) {
-          req.flash("success", `Welcome back ! ${foundUser.firstName}`);
-          res.redirect("/auth/signin");
-      }
-      else {
-          const hashedPassword = bcrypt.hashSync(newUser.password, 10);
-          newUser.password = hashedPassword;
-          await userModel.create(newUser);
-          req.flash("success", `Welcome ${foundUser.firstName}, please sign in `);
-          res.redirect("/auth/signin");
-      }  
+    const foundUser = await userModel.findOne({ email: newUser.email });
+
+    if (foundUser) {
+      req.flash("success", `Welcome back ! ${foundUser.firstName}`);
+      res.redirect("/auth/signin");
+    } else {
+      const hashedPassword = bcrypt.hashSync(newUser.password, 10);
+      newUser.password = hashedPassword;
+      await userModel.create(newUser);
+      req.flash("success", `Welcome ${foundUser.firstName}, please sign in `);
+      res.redirect("/auth/signin");
     }
-    catch (err) {
-      let errorMessage = "";
-      console.log(err);
-      for (field in err.errors) {
-          errorMessage += err.errors[field].message + "\n";
-      }
-      req.flash("error", errorMessage);
-      res.redirect("/auth/signup");
+  } catch (err) {
+    let errorMessage = "";
+    console.log(err);
+    for (field in err.errors) {
+      errorMessage += err.errors[field].message + "\n";
+    }
+    req.flash("error", errorMessage);
+    res.redirect("/auth/signup");
   }
   // next(err)
   // }
-})
+});
 
 router.get("/signout", (req, res, next) => {
-  req.session.destroy((err) => res.redirect("/auth/signin")
-  )
-})
-
-
+  req.session.destroy(err => res.redirect("/auth/signin"));
+});
 
 module.exports = router;
--- routes/users.route.js
+++ routes/users.route.js
@@ -5,15 +5,32 @@ const protecRoute = require("./../middlewares/protectRoute");
 
 router.use(protecRoute);
 
+router.get("/babysitters", (req, res, next) => {
+  res.render("users/babysittersList");
+});
+
+router.get("/families", (req, res, next) => {
+  res.render("users/familiesList");
+});
 
 router.get("/", (req, res, next) => {
-  
-  Users.find({ role:{$ne: req.session.currentUser.role}})
-    .then((users) => {
-      res.render("users/usersList", {
-        users: users,
-        css: ["users"],
-      });
+  //const role = req.session.currentUser.role; //babysitter
+  let userType = ""; //babysitter or family
+  console.log(req.session.currentUser.role);
+  Users.find({ role: { $ne: req.session.currentUser.role } })
+    .then(users => {
+      if (req.session.currentUser.role === "babysitter") {
+        res.render("users/familiesList", {
+          families: users,
+          css: ["users"]
+        });
+      } else if (req.session.currentUser.role === "family") {
+        res.render("users/babysittersList", {
+          babysitters: users,
+          css: ["users"]
+        });
+	  }
+	  else return  res.send("wrong user role !")
     })
     .catch(next);
 });
@@ -21,74 +38,69 @@ router.get("/", (req, res, next) => {
 router.get("/:id([a-f0-9]{24})", (req, res, next) => {
   console.log(req.params);
   const id = req.params.id;
-    Users.findById(id)
-      .then((user) => {
-        console.log(user);
+  Users.findById(id)
+    .then(user => {
+      console.log(user);
 
-        res.render("users/oneUser.hbs", {
-          user: user,
-          css: ["users"],
-        });
-      })
-      .catch(next);
+      res.render("users/oneUser.hbs", {
+        user: user,
+        css: ["users"]
+      });
+    })
+    .catch(next);
 });
 
 router.get("/create", (req, res, next) => {
   res.render("users/createUser.hbs");
 });
 
-router.post("/create", uploader.single("picture"), async (req, res,next) => {
-	const newUser = { ...req.body };
-	if (!req.file) newUser.picture = undefined;
-	else newUser.picture = req.file.path;
+router.post("/create", uploader.single("picture"), async (req, res, next) => {
+  const newUser = { ...req.body };
+  if (!req.file) newUser.picture = undefined;
+  else newUser.picture = req.file.path;
 
-	try {
-	await Users.create(newUser);
-	  console.log("NewUser: ", newUser);
-	  console.log("req.file >>",req.file);
-      res.redirect("/users");
-	}
-    catch(err) {
-		next(err);
-	}
+  try {
+    await Users.create(newUser);
+    console.log("NewUser: ", newUser);
+    console.log("req.file >>", req.file);
+    res.redirect("/users");
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/id/delete", async (req, res, next) => {
-	const id = req.params.id;
-	try {
-		await Users.findByIdAndDelete(id);
-		res.redirect("/users");
-	} catch (error) {
-		next(error);
-	}
+  const id = req.params.id;
+  try {
+    await Users.findByIdAndDelete(id);
+    res.redirect("/users");
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:id/update", (req, res, next) => {
-	const id = req.params.id;
-	
-	Users.findById(id)
-		.then((user) => {
-			res.render("users/updateUser.hbs", {
-				user: user,
-			});
-		})
-		.catch(next)
-	});
+  const id = req.params.id;
 
-
-
-router.post("/:id/update", uploader.single("picture"), (req, res, next) => {
-	const id = req.params.id;
-	const updatedUser = { ...req.body };
-	if (req.file) updatedUser.picture = req.file.path;
-	Users.findByIdAndUpdate(id, updatedUser, { new: true })
-		.then((updatedUser) => {
-			console.log(updatedUser);
-			res.redirect("/users/" + id);
-		})
-		.catch(next)
+  Users.findById(id)
+    .then(user => {
+      res.render("users/updateUser.hbs", {
+        user: user
+      });
+    })
+    .catch(next);
 });
 
-
+router.post("/:id/update", uploader.single("picture"), (req, res, next) => {
+  const id = req.params.id;
+  const updatedUser = { ...req.body };
+  if (req.file) updatedUser.picture = req.file.path;
+  Users.findByIdAndUpdate(id, updatedUser, { new: true })
+    .then(updatedUser => {
+      console.log(updatedUser);
+      res.redirect("/users/" + id);
+    })
+    .catch(next);
+});
 
 module.exports = router;
