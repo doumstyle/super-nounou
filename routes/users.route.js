5dd7432eed2615bf314e1115a0018f84d34021c5
Mamadou Ngatte <doumstyle@gmail.com>
2022-01-20 12:49:01 +0100

    Cleaning up

--- routes/users.route.js
+++ routes/users.route.js
@@ -3,20 +3,18 @@ const Users = require("./../models/users.model");
 const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
-  
-  Users.find({ role:{$ne: req.session.currentUser.role}})
+  Users.find({ role: { $ne: req.session.currentUser.role } })
     .then((users) => {
       res.render("users/usersList", {
         users: users,
         css: ["users", "sign"],
-        js: ["carousel"]
+        js: ["carousel"],
       });
     })
     .catch(next);
 });
 
 router.get("/:id", (req, res, next) => {
-  console.log(req.params);
   const isValidId = mongoose.isValidObjectId(req.params.id);
   const id = req.params.id;
   if (isValidId) {
@@ -37,13 +35,13 @@ router.get("/:id", (req, res, next) => {
 
 router.get("/create", (req, res, next) => {
   res.render("users/createUser.hbs", {
-    css: ["create"]
+    css: ["create"],
   });
 });
 
 router.post("/create", (req, res) => {
   console.log(req.body);
-	Users.create(req.body)
+  Users.create(req.body)
     .then((newUser) => {
       console.log("NewUser: ", newUser);
       res.redirect("/usersList");
@@ -52,36 +50,35 @@ router.post("/create", (req, res) => {
 });
 
 router.get("/id/delete", async (req, res) => {
-	const id = req.params.id;
-	try {
-		await Users.findByIdAndDelete(id);
-		res.redirect("/users");
-	} catch (error) {
-		console.error(error);
-	}
+  const id = req.params.id;
+  try {
+    await Users.findByIdAndDelete(id);
+    res.redirect("/users");
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 router.get("/:id/update", (req, res) => {
-	const id = req.params.id;
+  const id = req.params.id;
 
-	Users.findById(id)
-		.then((user) => {
-			res.render("users/updateUser.hbs", {
-				user: user,
-			});
-		})
-		.catch((e) => console.error(e));
+  Users.findById(id)
+    .then((user) => {
+      res.render("users/updateUser.hbs", {
+        user: user,
+      });
+    })
+    .catch((e) => console.error(e));
 });
 
 router.post("/:id/update", (req, res) => {
-	const id = req.params.id;
-	Users.findByIdAndUpdate(id, req.body, { new: true })
-		.then((updatedUser) => {
-			console.log(updatedUser);
-			res.redirect("/users/" + id);
-		})
-		.catch((e) => console.error(e));
+  const id = req.params.id;
+  Users.findByIdAndUpdate(id, req.body, { new: true })
+    .then((updatedUser) => {
+      console.log(updatedUser);
+      res.redirect("/users/" + id);
+    })
+    .catch((e) => console.error(e));
 });
-
 
 module.exports = router;
